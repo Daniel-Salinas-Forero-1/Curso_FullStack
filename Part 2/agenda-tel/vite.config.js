35fb3738ad1e205c858a43c560fb83e11a3b1601
Daniel-Salinas-Forero-1 <daniel.salinas@utp.edu.co>
2025-07-17 10:19:38 -0500

    fix: allow render.com host

--- Part 2/agenda-tel/vite.config.js
+++ Part 2/agenda-tel/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 10000,
     host: '0.0.0.0',
-    port: process.env.PORT || 5173
-  }
+    allowedHosts: ['curso-fullstack-qrz4.onrender.com'],
+  },
 })
