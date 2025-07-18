42700833129e38719a163a75c489b4adf8dde137
Daniel-Salinas-Forero-1 <daniel.salinas@utp.edu.co>
2025-07-17 19:26:34 -0500

    Subir a internet completo con redireccion de dist 3

--- Part 3/agenda-tel/index.js
+++ Part 3/agenda-tel/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors());
 app.use(express.json())
 app.use(morgan('tiny'))
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'public')));
 let persons = [
     { 
       "id": 1,
